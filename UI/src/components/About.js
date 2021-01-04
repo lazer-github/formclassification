49dd3694b28702a0b384f290dac83e301c265d6c
Arputha Lazer <Alazer@altimetrik.com>
2021-01-04 18:19:59 +0530

    updated about page

--- UI/src/components/About.js
+++ UI/src/components/About.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { about } from './../Static-data';
-
+import diagram from './../images/TechLinkImage.PNG'
 const About = () => {
-    return (<div className="flexbox-container">
-        <p>{about.objective}</p>
-        <br />
+    return (<div className="flexbox-container">        
+        <p>{about.objective}</p>  
+        <h4>Technology Map</h4>      
+        <img src={diagram} height={250} width={550} /> 
+        <h4>Technologies</h4>
         <ul>
             {about.technologies.map((item, i) => <li key={i}>{item}</li>)}
-        </ul>
-        <h4>Processing diagram</h4>
+        </ul>  
     </div>);
 }
 export default About
